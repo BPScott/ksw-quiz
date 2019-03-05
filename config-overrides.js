c92771af196289fe6a6a5472558b532ae589f35c
Ben Scott <ben@reload.me.uk>
2019-03-04 23:27:36 -0800

    Keep preact-compat

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,8 @@
 module.exports = function override(config) {
   // Use Preact instead of React
   config.resolve.alias = Object.assign(config.resolve.alias, {
-    react: 'preact',
-    'react-dom': 'preact',
+    react: 'preact-compat',
+    'react-dom': 'preact-compat',
   });
 
   return config;
