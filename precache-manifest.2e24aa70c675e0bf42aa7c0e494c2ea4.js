bbe5b6009e02c489171a05106b2dbc0d2f8f5c11
Ben Scott <ben@reload.me.uk>
2019-05-04 23:00:07 +0200

    Updates

---
+++ precache-manifest.2e24aa70c675e0bf42aa7c0e494c2ea4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e16b0c2e750a74b1e715b7f67ff68c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9abb2e12a3a9e853e83d",
+    "url": "/static/css/main.e623b08c.chunk.css"
+  },
+  {
+    "revision": "9abb2e12a3a9e853e83d",
+    "url": "/static/js/main.1dc48ed1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
