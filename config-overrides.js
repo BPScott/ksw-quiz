2f095637396eecc559d41d2344cb9e5871dabdd3
Ben Scott <ben@reload.me.uk>
2019-05-05 19:37:33 +0200

    Fix eslint-plugin-react errors
    Explicitly state a react version instead of trying to guess it from
    installed packages.
    
    - edit in package.json for editor integration
    - edit in config-overrides for the eslint config that runs as part of
      the build (which doesn't look at your usual eslint config in .eslintrc
      or package.json)

--- config-overrides.js
+++ config-overrides.js
@@ -5,5 +5,17 @@ module.exports = function override(config) {
     'react-dom': 'preact/compat',
   });
 
+  // Hint explicit react version to eslint to avoid warnings
+  // The default value of "detect" does not work as react is not a dependency
+  config.module.rules.forEach((rule) => {
+    (rule.use || []).forEach((useBlock) => {
+      if (useBlock.options && 'useEslintrc' in useBlock.options) {
+        useBlock.options.baseConfig.settings = {
+          react: {version: '16.8'},
+        };
+      }
+    });
+  });
+
   return config;
 };
